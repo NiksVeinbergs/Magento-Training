14f96403ab45ca9c75a140a000143cdbf710891b
NiksVeinbergs <niks.veinbergs@magebit.com>
2023-01-11 13:12:12 +0200

    CMS_Widget with Title and Display Mode properties

---
+++ app/code/Magebit/PageListWidget/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author       Niks Veinbergs
+ * @copyright    Copyright (c) 2023
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Magebit_PageListWidget',
+    __DIR__
+);
---
+++ app/code/Magebit/PageListWidget/view/frontend/templates/page-list.phtml
@@ -0,0 +1,5 @@
+<?php
+/**
+ * @author       Niks Veinbergs
+ * @copyright    Copyright (c) 2023
+ */
