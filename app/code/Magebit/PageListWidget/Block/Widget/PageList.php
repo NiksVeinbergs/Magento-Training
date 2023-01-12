e37ef438d694a727d7e884b8b22118e5c303a753
NiksVeinbergs <niks.veinbergs@magebit.com>
2023-01-12 10:49:32 +0200

    Something working in a wrong way.

--- app/code/Magebit/PageListWidget/Block/Widget/PageList.php
+++ app/code/Magebit/PageListWidget/Block/Widget/PageList.php
@@ -1,5 +1,31 @@
 <?php
 /**
+ * Magebit_PageListWidget
+ *
+ * @category     Magebit
+ * @package      Magebit_PageListWidget
  * @author       Niks Veinbergs
- * @copyright    Copyright (c) 2023
+ * @copyright    Copyright (c) 2023 Magebit, Ltd.(https://www.magebit.com/)
  */
+
+namespace Magebit\PageListWidget\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class PageList extends Template implements BlockInterface
+{
+    protected $_template = "page-list.phtml";
+    protected $_page;
+//Injectot page repository nevis source modeli
+    public function __construct(Template\Context $context, array $data = [], \Magento\Cms\Model\Config\Source\Page $page)
+    {
+        parent::__construct($context, $data);
+        $this->_page = $page;
+    }
+
+    public function pageOptionArray(): array
+    {
+        return $this->_page->toOptionArray();
+    }
+}
