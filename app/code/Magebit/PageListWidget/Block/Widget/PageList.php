9d7cc9465e15390157621ce87e7d19f8687701ff
NiksVeinbergs <niks.veinbergs@magebit.com>
2023-01-18 17:49:28 +0200

    First exercise fixes

--- app/code/Magebit/PageListWidget/Block/Widget/PageList.php
+++ app/code/Magebit/PageListWidget/Block/Widget/PageList.php
@@ -7,11 +7,17 @@
  * @author       Niks Veinbergs
  * @copyright    Copyright (c) 2023 Magebit, Ltd.(https://www.magebit.com/)
  */
+declare(strict_types=1);
 
 namespace Magebit\PageListWidget\Block\Widget;
 
+use Magento\Cms\Api\PageRepositoryInterface;
+use Magento\Cms\Helper\Page;
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
 use Magento\Widget\Block\BlockInterface;
 
 class PageList extends Template implements BlockInterface
@@ -20,63 +26,87 @@ class PageList extends Template implements BlockInterface
     protected \Magento\Cms\Api\PageRepositoryInterface $_pageRepository;
     protected \Magento\Cms\Helper\Page $_pageHelper;
     protected \Magento\Framework\Api\SearchCriteriaBuilder $_searchCriteriaBuilder;
+    protected \Magento\Framework\Api\FilterBuilder $_filterBuilder;
+    protected array $pages;
+    protected string $title;
+    protected string $displayMode;
 
+    /**
+     * @param Context $context
+     * @param Page $pageHelper
+     * @param PageRepositoryInterface $pageRepository
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param FilterBuilder $filterBuilder
+     * @param array $pages
+     * @param string $title
+     * @param string $displayMode
+     * @param array $data
+     */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \Magento\Cms\Helper\Page $pageHelper,
         \Magento\Cms\Api\PageRepositoryInterface $pageRepository,
         \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder,
+        \Magento\Framework\Api\FilterBuilder $filterBuilder,
+        array $pages = [],
+        string $title = '',
+        string $displayMode = '',
         array $data = []
     ) {
         parent::__construct($context, $data);
         $this->_pageRepository = $pageRepository;
         $this->_pageHelper = $pageHelper;
         $this->_searchCriteriaBuilder = $searchCriteriaBuilder;
-    }
-
-    public function getTitle()
-    {
-        return $this->getData('title');
+        $this->_filterBuilder = $filterBuilder;
+        $this->pages = $pages;
+        $this->title = $title;
+        $this->displayMode = $displayMode;
     }
 
     /**
+     * Short description.
+     *Returns input title as a String
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        $this->title = $this->getData('title');
+        return $this->title;
+    }
+
+    /**
+     * Short description.
+     * Returns display mode as a string
+     * @return string
+     */
+    public function getDisplayMode(): string
+    {
+        $this->displayMode = $this->getData('display_mode');
+        return $this->displayMode;
+    }
+
+    /**
+     * Short description.
+     *Based on display mode, returns specific pages (adds additional filter) or if display mode is == 1 returns all pages w/o filter
+     * @return array
      * @throws LocalizedException
      */
     public function getPages(): array
     {
-        if (empty($this->getData('pages'))) {
-            return $this->getAllPages();
+        $filters =[];
+        if ($this->getDisplayMode() == '2') {
+            $selectedPageIds = explode(',', $this->getData('pages'));
+            $filters[] = $this->_filterBuilder->setField('identifier')->setValue($selectedPageIds)->setConditionType('in')->create();
         }
-        $selectedPageIds = explode(',', $this->getData('pages'));
-        $pages = [];
-        foreach ($selectedPageIds as $pageId) {
-            try {
-                $page = $this->_pageRepository->getById($pageId);
-                $pages[] = [
-                    'url' => $this->_pageHelper->getPageUrl($pageId),
-                    'name' => $page->getTitle()
-                ];
-            } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
-                // page not found
-            }
-        }
-        return $pages;
-    }
 
-    /**
-     * @throws LocalizedException
-     */
-    public function getAllPages(): array
-    {
-        $searchCriteria = $this->_searchCriteriaBuilder->create();
+        $searchCriteria = $this->_searchCriteriaBuilder->addFilters($filters)->create();
         $searchResults = $this->_pageRepository->getList($searchCriteria);
-        $pages = [];
         foreach ($searchResults->getItems() as $page) {
-            $pages[] = [
+            $this->pages[] = [
                 'url' => $this->_pageHelper->getPageUrl($page->getId()),
                 'name' => $page->getTitle()
             ];
         }
-        return $pages;
+        return $this->pages;
     }
 }
--- app/code/Magebit/PageListWidget/registration.php
+++ app/code/Magebit/PageListWidget/registration.php
@@ -3,6 +3,7 @@
  * @author       Niks Veinbergs
  * @copyright    Copyright (c) 2023
  */
+declare(strict_types=1);
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
