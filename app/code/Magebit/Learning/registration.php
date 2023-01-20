707e7efcde41752f15107d16861f3fbda42bf808
NiksVeinbergs <niks.veinbergs@magebit.com>
2023-01-20 17:27:48 +0200

    Fixed broken products and added module.xml and registration.php files to Magebit_Learning module

---
+++ app/code/Magebit/Learning/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author       Niks Veinbergs
+ * @copyright    Copyright (c) 2023
+ */
+declare(strict_types=1);
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Magebit_Learning',
+    __DIR__
+);
