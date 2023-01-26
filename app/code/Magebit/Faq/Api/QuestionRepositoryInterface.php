1c0c008e0e4790196a23da2264747e8e0457a510
NiksVeinbergs <niks.veinbergs@magebit.com>
2023-01-26 17:45:21 +0200

    QuestionRepository.php and QuestionRepositoryInterface.php formatted

--- app/code/Magebit/Faq/Api/QuestionRepositoryInterface.php
+++ app/code/Magebit/Faq/Api/QuestionRepositoryInterface.php
@@ -7,16 +7,61 @@
  * @author       Niks Veinbergs
  * @copyright    Copyright (c) 2023 Magebit, Ltd.(https://www.magebit.com/)
  */
-
+declare(strict_types=1);
 namespace Magebit\Faq\Api;
 
+use Magebit\Faq\Model\Question;
 use Magento\Framework\Api\SearchCriteriaInterface;
 
+/**
+ * Interface QuestionRepositoryInterface
+ */
 interface QuestionRepositoryInterface
 {
-    public function getById($questionId);
-    public function save(Data\QuestionInterface $question);
-    public function getList(SearchCriteriaInterface $searchCriteria);
-    public function delete(Data\QuestionInterface $question);
-    public function deleteById($questionId);
+
+    /**
+     * Description.
+     * Returns Question, based on given Question ID
+     *
+     * @param string $questionId
+     * @return Question
+     */
+    public function getById(string $questionId): Question;
+
+    /**
+     * Description.
+     *Saves question
+     *
+     * @param Data\QuestionInterface $question
+     * @return Data\QuestionInterface
+     */
+    public function save(Data\QuestionInterface $question): Data\QuestionInterface;
+
+    /**
+     * Description.
+     *Get List of Questions based on given search criteria
+     *
+     * @param SearchCriteriaInterface $searchCriteria
+     * @return array
+     */
+    public function getList(SearchCriteriaInterface $searchCriteria): array;
+
+
+    /**
+     * Description.
+     *Delete question
+     *
+     * @param Data\QuestionInterface $question
+     * @return bool
+     */
+    public function delete(Data\QuestionInterface $question): bool;
+
+    /**
+     * Description.
+     * Delete Question by given id
+     *
+     * @param int $questionId
+     * @return bool
+     */
+    public function deleteById(int $questionId): bool;
 }
--- app/code/Magebit/Faq/Model/QuestionRepository.php
+++ app/code/Magebit/Faq/Model/QuestionRepository.php
@@ -7,32 +7,64 @@
  * @author       Niks Veinbergs
  * @copyright    Copyright (c) 2023 Magebit, Ltd.(https://www.magebit.com/)
  */
-
+declare(strict_types=1);
 namespace Magebit\Faq\Model;
 
 use Magebit\Faq\Api\Data;
 use Magebit\Faq\Api\QuestionRepositoryInterface;
 use Magebit\Faq\Model\ResourceModel\Question as ResourceQuestion;
 use Magebit\Faq\Model\ResourceModel\Question\CollectionFactory as QuestionCollectionFactory;
+use Magento\Framework\Api\ExtensibleDataInterface;
 use Magento\Framework\Api\SearchCriteria\CollectionProcessorInterface;
 use Magento\Framework\Api\SearchCriteriaInterface;
 use Magento\Framework\Exception\CouldNotDeleteException;
 use Magento\Framework\Exception\CouldNotSaveException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magebit\Faq\Api\Data\QuestionSearchResultsInterfaceFactory;
+use Magebit\Faq\Api\Data\QuestionInterfaceFactory;
 
+/**
+ * Class QuestionRepository
+ */
 class QuestionRepository implements QuestionRepositoryInterface
 {
+    /**
+     * @var ResourceQuestion
+     */
     protected $resource;
+    /**
+     * @var QuestionFactory
+     */
     protected $questionFactory;
+    /**
+     * @var QuestionCollectionFactory
+     */
     protected $questionCollectionFactory;
+    /**
+     * @var QuestionSearchResultsInterfaceFactory
+     */
     protected $searchResultsFactory;
+    /**
+     * @var QuestionInterfaceFactory
+     */
+    protected $dataQuestionFactory;
+    /**
+     * @var CollectionProcessorInterface
+     */
     private $collectionProcessor;
 
+    /**
+     * @param ResourceQuestion $resource
+     * @param QuestionFactory $questionFactory
+     * @param QuestionInterfaceFactory $dataQuestionFactory
+     * @param QuestionCollectionFactory $questionCollectionFactory
+     * @param QuestionSearchResultsInterfaceFactory $searchResultsFactory
+     * @param CollectionProcessorInterface $collectionProcessor
+     */
     public function __construct(
         ResourceQuestion $resource,
         QuestionFactory $questionFactory,
-        \Magebit\Faq\Api\Data\QuestionInterfaceFactory $dataQuestionFactory,
+        QuestionInterfaceFactory $dataQuestionFactory,
         QuestionCollectionFactory $questionCollectionFactory,
         QuestionSearchResultsInterfaceFactory $searchResultsFactory,
         CollectionProcessorInterface $collectionProcessor
@@ -46,12 +78,19 @@ class QuestionRepository implements QuestionRepositoryInterface
         $this->collectionProcessor = $collectionProcessor;
     }
 
+
     /**
+     * Description.
+     * Returns Question, based on given Question ID
+     *
+     * @param string $questionId
+     * @return Question
      * @throws NoSuchEntityException
      */
-    public function getById($questionId): Question
+    public function getById(string $questionId): Question
     {
         $question = $this->questionFactory->create();
+        $questionId = (int) $questionId; //TODO Maybe other way how to pass INT straight from frontend not string
         $this->resource->load($question, $questionId);
         if (!$question->getId()) {
             throw new NoSuchEntityException(__('The Question with the "%1" ID doesn\'t exist.', $questionId));
@@ -59,8 +98,15 @@ class QuestionRepository implements QuestionRepositoryInterface
         return $question;
     }
 
+
     /**
+     * Description.
+     *Saves question
+     *
+     * @param Data\QuestionInterface $question
+     * @return Data\QuestionInterface
      * @throws CouldNotSaveException
+     * @throws \Magento\Framework\Exception\AlreadyExistsException
      */
     public function save(Data\QuestionInterface $question): Data\QuestionInterface
     {
@@ -72,10 +118,16 @@ class QuestionRepository implements QuestionRepositoryInterface
         return $question;
     }
 
-    public function getList(SearchCriteriaInterface $searchCriteria)
+    /**
+     * Description.
+     *Get List of Questions based on given search criteria
+     *
+     * @param SearchCriteriaInterface $searchCriteria
+     * @return ExtensibleDataInterface[]
+     */
+    public function getList(SearchCriteriaInterface $searchCriteria): array
     {
         $collection = $this->questionCollectionFactory->create();
-
         $this->collectionProcessor->process($searchCriteria, $collection);
 
         $searchResults = $this->searchResultsFactory->create();
@@ -85,6 +137,14 @@ class QuestionRepository implements QuestionRepositoryInterface
         return $searchResults->getItems();
     }
 
+    /**
+     * Description.
+     *Delete question
+     *
+     * @param Data\QuestionInterface $question
+     * @return bool
+     * @throws CouldNotDeleteException
+     */
     public function delete(Data\QuestionInterface $question): bool
     {
         try {
@@ -95,11 +155,17 @@ class QuestionRepository implements QuestionRepositoryInterface
         return true;
     }
 
+
     /**
+     * Description.
+     * Delete Question by given id
+     *
+     * @param int $questionId
+     * @return bool
      * @throws CouldNotDeleteException
      * @throws NoSuchEntityException
      */
-    public function deleteById($questionId): bool
+    public function deleteById(int $questionId): bool
     {
         $question = $this->questionFactory->create();
         $this->resource->load($question, $questionId);
