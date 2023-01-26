ab98784b633c7aeec07fd140c4c6b3b5ae53b1cd
NiksVeinbergs <niks.veinbergs@magebit.com>
2023-01-26 14:15:09 +0200

    QuestionManagement.php and QuestionManagementInterface.php formatted

--- app/code/Magebit/Faq/Api/QuestionManagementInterface.php
+++ app/code/Magebit/Faq/Api/QuestionManagementInterface.php
@@ -7,15 +7,19 @@
  * @author       Niks Veinbergs
  * @copyright    Copyright (c) 2023 Magebit, Ltd.(https://www.magebit.com/)
  */
-
+declare(strict_types=1);
 namespace Magebit\Faq\Api;
 
 use Magebit\Faq\Model\Question;
 
 interface QuestionManagementInterface
 {
+    /**#@+
+     * Constants for Status (Enabled or Disabled)
+     */
     const STATUS_ENABLED = 1;
     const STATUS_DISABLED = 0;
+    /**#@-*/
     public function enableQuestion(Question $question);
     public function disableQuestion(Question $question);
 }
--- app/code/Magebit/Faq/Model/QuestionManagement.php
+++ app/code/Magebit/Faq/Model/QuestionManagement.php
@@ -7,32 +7,55 @@
  * @author       Niks Veinbergs
  * @copyright    Copyright (c) 2023 Magebit, Ltd.(https://www.magebit.com/)
  */
-
+declare(strict_types=1);
 namespace Magebit\Faq\Model;
 
 use Magebit\Faq\Api\QuestionManagementInterface;
 use Magento\Framework\Exception\CouldNotSaveException;
 
+/**
+ * Class QuestionManagement
+ */
 class QuestionManagement implements QuestionManagementInterface
 {
+    /**
+     * @var QuestionRepository
+     */
     private $questionRepository;
+
+    /**
+     * @param QuestionRepository $questionRepository
+     */
     public function __construct(
         QuestionRepository $questionRepository
     ) {
         $this->questionRepository = $questionRepository;
     }
 
+
     /**
+     * Description.
+     * Disable question, sets status value to STATUS_ENABLED and makes it visible in front-end
+     *
+     * @param Question $question
+     * @return void
      * @throws CouldNotSaveException
      */
-    public function enableQuestion(Question $question)
+    public function enableQuestion(Question $question) : void
     {
         $question->setStatus(self::STATUS_ENABLED);
         $this->questionRepository->save($question);
-
     }
 
-    public function disableQuestion(Question $question)
+    /**
+     * Description.
+     * Disables question, sets status value to STATUS_DISABLED and makes it invisible in front-end
+     *
+     * @param Question $question
+     * @return void
+     * @throws CouldNotSaveException
+     */
+    public function disableQuestion(Question $question): void
     {
         $question->setStatus(self::STATUS_DISABLED);
         $this->questionRepository->save($question);
