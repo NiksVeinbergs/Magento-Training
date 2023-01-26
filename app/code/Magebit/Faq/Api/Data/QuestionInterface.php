f1bb4cd83275d9c7389853a88c234ccb8f9f87b0
NiksVeinbergs <niks.veinbergs@magebit.com>
2023-01-26 14:07:10 +0200

    Question and QuestionInterface formated

--- app/code/Magebit/Faq/Api/Data/QuestionInterface.php
+++ app/code/Magebit/Faq/Api/Data/QuestionInterface.php
@@ -7,26 +7,31 @@
  * @author       Niks Veinbergs
  * @copyright    Copyright (c) 2023 Magebit, Ltd.(https://www.magebit.com/)
  */
-
+declare(strict_types=1);
 namespace Magebit\Faq\Api\Data;
 
 interface QuestionInterface
 {
+    /**#@+
+     * Constants for keys of data array.
+     */
     const QUESTION_ID = 'id';
     const QUESTION_TITLE = 'question';
     const QUESTION_ANSWER = 'answer';
     const QUESTION_STATUS = 'status';
     const QUESTION_POSITION = 'position';
     const UPDATED_AT = 'updated_at';
+    /**#@-*/
+
     public function getId();
     public function getQuestion();
-    public function setQuestion($question);
+    public function setQuestion(string $question);
     public function getAnswer();
-    public function setAnswer($answer);
+    public function setAnswer(string $answer);
 
     public function getStatus();
-    public function setStatus($status);
+    public function setStatus(int $status);
     public function getPosition();
-    public function setPosition($position);
+    public function setPosition(int $position);
     public function getUpdatedAt();
 }
--- app/code/Magebit/Faq/Model/Question.php
+++ app/code/Magebit/Faq/Model/Question.php
@@ -7,7 +7,6 @@
  * @author       Niks Veinbergs
  * @copyright    Copyright (c) 2023 Magebit, Ltd.(https://www.magebit.com/)
  */
-
 declare(strict_types=1);
 namespace Magebit\Faq\Model;
 
@@ -17,57 +16,127 @@ use Magento\Framework\Model\AbstractModel;
 
 class Question extends AbstractModel implements QuestionInterface
 {
-    protected function _construct()
+    /**
+     * Description.
+     *Constructor for Question Model
+     *
+     * @return void
+     */
+    protected function _construct(): void
     {
         $this->_init(QuestionResource::class);
     }
 
-    public function getId()
+    /**
+     * Description.
+     *Get ID
+     *
+     * @return array|mixed|null
+     */
+    public function getId(): mixed
     {
         return $this->getData(self::QUESTION_ID);
     }
 
-    public function getQuestion()
+    /**
+     * Description.
+     * Get FAQ title/question name
+     *
+     * @return array|mixed|null
+     */
+    public function getQuestion(): mixed
     {
         return $this->getData(self::QUESTION_TITLE);
     }
 
-    public function setQuestion($question): Question
+    /**
+     * Description.
+     *Set FAQ title/question name
+     *
+     * @param string $question
+     * @return Question
+     */
+    public function setQuestion(string $question): Question
     {
         return $this->setData(self::QUESTION_TITLE, $question);
     }
 
-    public function getAnswer()
+    /**
+     * Description.
+     *Get answer
+     *
+     * @return array|mixed|null
+     */
+    public function getAnswer(): mixed
     {
         return $this->getData(self::QUESTION_ANSWER);
     }
 
-    public function setAnswer($answer): Question
+    /**
+     * Description.
+     *Set answer
+     *
+     * @param string $answer
+     * @return Question
+     */
+    public function setAnswer(string $answer): Question
     {
         return $this->setData(self::QUESTION_ANSWER, $answer);
     }
 
-    public function getStatus()
+    /**
+     * Description.
+     *Get status
+     *
+     * @return array|mixed|null
+     */
+    public function getStatus(): mixed
     {
         return $this->getData(self::QUESTION_STATUS);
     }
 
-    public function setStatus($status): Question
+    /**
+     * Description.
+     *Set status
+     *
+     * @param int $status
+     * @return Question
+     */
+    public function setStatus(int $status): Question
     {
         return $this->setData(self::QUESTION_STATUS, $status);
     }
 
-    public function getPosition()
+    /**
+     * Description.
+     *Get position
+     *
+     * @return array|mixed|null
+     */
+    public function getPosition(): mixed
     {
         return $this->getData(self::QUESTION_POSITION);
     }
 
-    public function setPosition($position): Question
+    /**
+     * Description.
+     *Set position (higher position - FAQ will append higher)
+     *
+     * @param int $position
+     * @return Question
+     */
+    public function setPosition(int $position): Question
     {
         return $this->setData(self::QUESTION_POSITION, $position);
     }
 
-    public function getUpdatedAt()
+    /**
+     * Description.
+     *Get last time question was updated
+     *
+     * @return array|mixed|null
+     */
+    public function getUpdatedAt(): mixed
     {
         return $this->getData(self::UPDATED_AT);
     }
