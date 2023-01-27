8ad77ec2927145cef0242a22adf654a20cb54438
NiksVeinbergs <niks.veinbergs@magebit.com>
2023-01-27 11:07:34 +0200

    FrontEnd controller file

--- app/code/Magebit/Faq/Controller/Index/Index.php
+++ app/code/Magebit/Faq/Controller/Index/Index.php
@@ -11,15 +11,31 @@ declare(strict_types=1);
 namespace Magebit\Faq\Controller\Index;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\Page;
 use Magento\Framework\View\Result\PageFactory;
 
+/**
+ * Class Index
+ */
 class Index implements HttpGetActionInterface
 {
+    /**
+     * @param PageFactory $pageFactory
+     */
     public function __construct(
         private PageFactory $pageFactory
     ) {
     }
-    public function execute()
+
+    /**
+     * Description.
+     *Returns front-end page of FAQ and sets page title and layout to 2 columns
+     *
+     * @return ResponseInterface|ResultInterface|Page
+     */
+    public function execute() //TODO Return type?
     {
         $page = $this->pageFactory->create();
         $page->getConfig()->getTitle()->set(__('Frequently Asked Questions'));
